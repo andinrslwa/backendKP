7fbb14cf9a88b091aab2529696e52fdced5a5fe0
unknown <helyxrulk@gmail.com>
2024-10-11 00:04:29 +0700

    add Cors

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const app = express()
-
+const cors = require("cors")
 
 const port = 5000
 
 // Middleware
 app.use(express.json())
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.status(200).json({
